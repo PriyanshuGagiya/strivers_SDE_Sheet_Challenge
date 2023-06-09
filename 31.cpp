bb513b728ed72bea285f5cba183c2c52bc5361f8
Priyanshu Gagiya <90676444+PriyanshuGagiya@users.noreply.github.com>
2023-06-09 19:34:51 +0530

    Update 31.cpp

--- 31.cpp
+++ 31.cpp
@@ -20,3 +20,84 @@ Node* findIntersection(Node *firstHead, Node *secondHead)
     }
     return NULL;
 }
+
+//better approach
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+*****************************************************************/
+#include <bits/stdc++.h>
+Node* findIntersection(Node *firstHead, Node *secondHead)
+{
+    
+   Node* l1=firstHead;
+    Node * l2=secondHead;
+    int cnt1=0,cnt2=0;
+    while (l1 )
+    {
+        cnt1++;
+        l1=l1->next;
+    }
+    while (l2)
+    {
+        cnt2++;
+        l2=l2->next;
+    }
+    l1=firstHead;
+    l2=secondHead;
+    if (cnt1>cnt2)
+    {
+        int n=cnt1-cnt2;
+        while (n--)
+        {
+            l1=l1->next;
+        }
+    }
+    else if (cnt1<cnt2)
+    {
+
+        int n=cnt2-cnt1;
+        while (n--)
+        {
+            l2 = l2->next;
+        }
+
+    }
+    
+    while (l1 && l2)
+    {
+        if (l1==l2)
+        {
+            return l1;
+        }
+        else
+        {
+            l1=l1->next;
+            l2=l2->next;
+        }
+    }
+    return NULL;
+}
