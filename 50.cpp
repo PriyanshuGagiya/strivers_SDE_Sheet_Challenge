f0462586f8b7ca4aa839d7029a1d112fd4be1004
Priyanshu Gagiya <90676444+PriyanshuGagiya@users.noreply.github.com>
2023-06-11 16:41:40 +0530

    Create 50.cpp

---
+++ 50.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+//https://www.codingninjas.com/codestudio/problems/subset-sum_8230859?challengeSlug=striver-sde-challenge
+void solve(int idx,vector<int>&arr,vector<int> &num,set<vector<int>>&ans)
+{
+        if(idx == arr.size())
+        {
+            vector<int>temp=num;
+            sort(temp.begin(),temp.end());
+            ans.insert(temp);
+            return;
+        }
+        num.push_back(arr[idx]);
+        solve(idx+1,arr,num, ans);
+        num.pop_back();
+        solve(idx+1,arr,num, ans);
+}
+vector<vector<int>> uniqueSubsets(int n, vector<int> &arr)
+{
+    // Write your code here.
+    vector<int>temp;
+    set<vector<int>>ans;
+    solve(0,arr,temp,ans);
+    vector<vector<int>>res(ans.begin(),ans.end());
+    return res;
+}
