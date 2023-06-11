ed92df4cfd4b734d40f0b67d47185ad17c1f7e1e
Priyanshu Gagiya <90676444+PriyanshuGagiya@users.noreply.github.com>
2023-06-11 15:33:14 +0530

    Update 44.cpp

--- 44.cpp
+++ 44.cpp
@@ -27,3 +27,22 @@ int calculateMinPatforms(int at[], int dt[], int n)
    return ans;
 
 }
+//o(1) space
+int calculateMinPatforms(int at[], int dt[], int n) {
+    // Write your code here.
+    sort(at,at+n);
+    sort(dt,dt+n);
+    int cnt = 0,ans=0;
+    int i=0,j=0;
+    while(i<n && j<n){
+        if(at[i]<=dt[j]){
+            cnt++;
+            i++;
+        }else{
+            cnt--;
+            j++;
+        }
+        ans=max(ans,cnt);
+    }
+    return ans;
+}
