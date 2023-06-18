36644ef7825eb5d9932d320ac4c3dc31c4753bb1
Priyanshu Gagiya <90676444+PriyanshuGagiya@users.noreply.github.com>
2023-06-18 19:44:54 +0530

    Update 74.cpp

--- 74.cpp
+++ 74.cpp
@@ -22,3 +22,27 @@ vector<int> KMostFrequent(int n, int k, vector<int> &arr)
     sort(ans.begin(),ans.end());
     return ans;
 }
+//
+#include <bits/stdc++.h> 
+vector<int> KMostFrequent(int n, int k, vector<int> &arr)
+{
+    unordered_map<int,int>mp;
+    for(int i=0;i<n;i++)
+    {
+        mp[arr[i]]++;
+    }
+    priority_queue<pair<int,int>>pq;
+    for(auto it:mp)
+    {
+        pq.push({it.second,it.first});
+    }
+    mp.clear();
+    vector<int>ans;
+    for(int i=0;i<k;i++)
+    {
+        ans.push_back(pq.top().second);
+        pq.pop();
+    }
+    sort(ans.begin(),ans.end());
+    return ans;
+}
