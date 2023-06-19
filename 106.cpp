57031b6a750d6c00b2897490b5da3459e997858a
Priyanshu Gagiya <90676444+PriyanshuGagiya@users.noreply.github.com>
2023-06-19 15:00:15 +0530

    Update 106.cpp

--- 106.cpp
+++ 106.cpp
@@ -1,5 +1,114 @@
 #include <bits/stdc++.h> 
 //https://www.codingninjas.com/codestudio/problems/find-pattern-in-string-kmp-algorithm_8230819?challengeSlug=striver-sde-challenge&leftPanelTab=1
+//kmp
+#include <bits/stdc++.h> 
+
+void lps(string p,vector<int>&temp,int pS)
+
+{
+
+    temp[0]=0;
+
+    int i=0;
+
+    int j=1;
+
+    while(j<pS)
+
+    {
+
+        if(p[i]==p[j])
+
+        {
+
+            i++;
+
+            temp[j]=i;
+
+            j++;
+
+        }
+
+        else
+
+        {
+
+            if(i!=0)
+
+            i=temp[i-1];
+
+            else
+
+            {
+
+                temp[j]=0;
+
+                j++;
+
+            }
+
+        }
+
+    }
+
+}
+
+bool findPattern(string p, string s)
+
+{
+
+    // Write your code here.
+
+    int pS=p.size();
+
+    int sS=s.size();
+
+    vector<int>temp(pS);
+
+    lps(p,temp,pS);
+
+    int i=0;
+
+    int j=0;
+
+    while(j<sS)
+
+    {
+
+        if(s[j]==p[i])
+
+        {
+
+            i++;
+
+            j++;
+
+        }
+
+        if(i==pS)
+
+        return true;
+
+        else if(j<sS && s[j]!=p[i])
+
+        {
+
+            if(i!=0)
+
+            i=temp[i-1];
+
+            else
+
+            j++;
+
+        }
+
+    }
+
+    return false;
+
+}
+//methid that i used
 bool findPattern(string p, string s)
 {
     // Write your code here.
