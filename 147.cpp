d9a56b369acd4372edc8f0258b1dd3795027a234
Priyanshu Gagiya <90676444+PriyanshuGagiya@users.noreply.github.com>
2023-06-20 18:06:32 +0530

    Update 147.cpp

--- 147.cpp
+++ 147.cpp
@@ -58,3 +58,110 @@ bool pairSumBst(BinaryTreeNode<int> *root, int k)
     }
     return 0;
 }
+// better approach
+#include <bits/stdc++.h> 
+/**********************************************************
+
+    Following is the Binary Tree Node structure:
+
+    template <typename T>
+    class BinaryTreeNode {
+        public: 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~BinaryTreeNode() {
+            if (left)
+                delete left;
+            if (right)
+                delete right;
+        }
+    };
+***********************************************************/
+class BSTiterator
+{
+    private: stack<BinaryTreeNode<int>*>st;
+    public:
+    bool rev=1;
+    BSTiterator(BinaryTreeNode<int> *root,bool r)
+    {
+        // write your code here
+        pushAll(root);
+        rev=r;
+    }
+
+    int next()
+    {
+        if(rev)
+        {
+            auto it=st.top();
+            st.pop();
+            if(it->left)
+            {
+                pushAll(it->left);
+            }
+            return it->data;
+        }
+        else
+        {
+        auto it=st.top();
+        st.pop();
+        if (it->right) {
+          pushAll(it->right);
+        }
+        return it->data;
+        }
+    }
+
+    void pushAll(BinaryTreeNode<int>*root)
+    {
+        if(rev)
+        {
+            while (root) 
+            {
+                st.push(root);
+                root = root->right;
+            }
+        }
+        else
+        {
+          while (root) {
+            st.push(root);
+            root = root->left;
+          }
+        }
+    }
+};
+bool pairSumBst(BinaryTreeNode<int> *root, int k)
+{
+    // Write your code here
+    BSTiterator l(root,false);
+    BSTiterator h(root,true);
+    int lo=l.next();
+    int hi=h.next();
+    while(lo<hi)
+    {
+        int sum=lo+hi;
+        if(sum==k)
+        {
+            return 1;
+        }
+        if(sum<k)
+        {
+            lo=l.next();;
+        }
+        else
+        {
+            hi=h.next();
+        }
+
+    }
+    return 0;
+}
