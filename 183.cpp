5a99f4df485526e4f7cdbb16c054bf10838eb9d9
Priyanshu Gagiya <90676444+PriyanshuGagiya@users.noreply.github.com>
2023-06-23 17:28:12 +0530

    Update 183.cpp

--- 183.cpp
+++ 183.cpp
@@ -33,3 +33,35 @@ bool wordBreak(vector < string > & arr, int n, string & target)
     solve(0,target.size(),target,mp,dp);
     return dp[0];
 }
+//better approach
+#include <bits/stdc++.h> 
+int solve(int i,int n,string&target,vector < string > & arr,vector<int>&dp)
+{
+    if(i==n)
+    {
+        return 1;
+    }
+    if(dp[i]!=-1) return dp[i];
+    string curr="";
+    for(int j=i;j<n;j++)
+    {
+        curr+=target[j];
+        auto it=lower_bound(arr.begin(),arr.end(),curr);
+        if(it!=arr.end() and (*it)==curr)
+        {
+            if(solve(j+1,n,target,arr,dp))
+            {
+                return 1;
+            }
+        }
+    }
+    return dp[i]=0;
+}
+bool wordBreak(vector < string > & arr, int n, string & target) 
+{
+    // Write your code here.
+    sort(arr.begin(),arr.end());
+    vector<int>dp(target.size(),-1);
+    solve(0,target.size(),target,arr,dp);
+    return dp[0];
+}
